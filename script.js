08f19b2c077d8b9403f82eda9f76d9cad1ff0fb1
Op Pautu <pautuop@gmail.com>
2022-07-24 18:54:49 +0530

    add event listener and expand cards

--- script.js
+++ script.js
@@ -2,13 +2,18 @@ const panels = document.querySelectorAll('.panel');
 console.log(panels)
 panels.forEach(panel => {
     panel.addEventListener('click', () => {
-        
+        removeActive()
         panel.classList.add('active')
+        
 
     })
 })
 
+function removeActive() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+})
 
-function() {
     
+
 }
