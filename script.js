2fb99214cb29d29134965c871fc87ed52372a7fb
Op Pautu <pautuop@gmail.com>
2022-07-24 17:28:14 +0530

    add style and flex grow

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const panels = document.querySelectorAll('.panel');
+console.log(panels)
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        
+        panel.classList.add('active')
 
+    })
+})
+
+
+function() {
+    
+}
